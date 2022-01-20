e18243b80cd2ddcd8eef850c80e41c24bbacb1d5
KamilChabros <kamilchabros@wp.pl>
2022-01-20 20:28:30 +0100

    Created database localy employeemanager to connect; EmployeeRepo Interface created; Created package exception for UserNoFoundException;

---
+++ src/main/java/xyz/getarrays/employeemanager/repositories/EmployeeRepo.java
@@ -0,0 +1,12 @@
+package xyz.getarrays.employeemanager.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import xyz.getarrays.employeemanager.model.Employee;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
